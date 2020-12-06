55743d06397eb61f9e82d13edde9a691559f56fe
Susan Wright <wright1@ualberta.ca>
2020-12-05 22:17:13 -0700

    Solution for day 6

---
+++ 06/sol.rb
@@ -0,0 +1,14 @@
+require "pry"
+
+inputs = File.read("input.txt").split("\n\n")
+
+part1 = 0
+part2 = 0
+
+inputs.each do |group|
+  part1 += group.chars.reject { |x| x == "\n" }.uniq.count
+  part2 += group.split("\n").map(&:chars).reduce { |x, y| x & y }.size
+end
+
+puts part1
+puts part2
